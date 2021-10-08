0a72baa7dd75edb74d0062b7fe6c172669861fbc
harsh6975 <harsh.sinha@iitg.ac.in>
2021-10-08 13:18:37 +0530

    connected to databse

--- config/database.js
+++ config/database.js
@@ -1,15 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/codeial_development');
+mongoose.connect("mongodb://localhost/codeial_development");
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
-
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
+db.once("open", function () {
+  console.log("Connected to Database :: MongoDB");
 });
-
 
 module.exports = db;
