5a8f617a28efacce5cbe9f4403141f41ba70a76a
harsh6975 <harsh.sinha@iitg.ac.in>
2021-10-16 12:39:48 +0530

    displayed all users on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require("../models/postSchema");
+const User = require("../models/userSchema");
 
 module.exports.home = function (req, res) {
   //To get data from other models
@@ -16,9 +17,16 @@ module.exports.home = function (req, res) {
         console.log("error in finding the post ", err);
         return;
       }
-      return res.render("home", {
-        title: "CodeBooks",
-        postList: posts,
+      User.find({}, function (err, user) {
+        if (err) {
+          console.log("err in findeing user");
+          return;
+        }
+        return res.render("home", {
+          title: "CodeBooks",
+          postList: posts,
+          user_friend: user,
+        });
       });
     });
 };
