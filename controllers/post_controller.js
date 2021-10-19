5e9dfb798c0a603c7e29ed49a80b53a035ddcc6b
harsh6975 <harsh.sinha@iitg.ac.in>
2021-10-19 20:53:29 +0530

    ajax for post

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,23 +1,30 @@
 const Post = require("../models/postSchema");
 const Comment = require("../models/commentSchema");
 //controller for post
-module.exports.create_post = function (req, res) {
+module.exports.create_post = async function (req, res) {
   // console.log(req.body);
   //console.log(req.user);
-  Post.create(
-    {
+  try {
+    let post = await Post.create({
       content: req.body.content,
       user: req.user._id,
-    },
-    function (err, post) {
-      if (err) {
-        req.flash('err',err);
-        return;
-      }
-      req.flash('sucess','Post sucessfully created');
-      return res.redirect("back");
+    });
+
+    if (req.xhr) {
+      return res.status(200).json({
+        data: {
+          post: post,
+        },
+        message: "Post created!",
+      });
     }
-  );
+
+    req.flash("sucess", "Post sucessfully created");
+    return res.redirect("back");
+  } catch (err) {
+    req.flash("error", err);
+    return res.redirect("back");
+  }
 };
 
 module.exports.destroy = async function (req, res) {
@@ -58,7 +65,7 @@ module.exports.destroy = async function (req, res) {
     let post = await Post.findById(req.query.id);
     post.remove();
     await Comment.deleteMany({ post: req.query.id });
-    req.flash('sucess','Post sucessfully Deleted');
+    req.flash("sucess", "Post sucessfully Deleted");
     return res.redirect("back");
   } catch (err) {
     console.log("err in deleting the post");
